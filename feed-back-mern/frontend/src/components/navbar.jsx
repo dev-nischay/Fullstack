5ac103d0b3726ff565e0bf1a87ccd2c65a02599c
dev-nischay <nischay.mehn@gmail.com>
2025-09-28 20:04:04 +0530

    added components

---
+++ feed-back-mern/frontend/src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "/rocket-lunch.svg"
+
+
+export default () =>{
+    return(
+        <>
+        <div className="navbar">
+        <img className="logo" src={logo} alt="" />
+        <div className="title">FeedBackHub</div>
+        </div>
+        </>
+    )
+}
