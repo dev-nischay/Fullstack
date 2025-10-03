e06c09f6390cbbafff2650e9750081c9683effbf
dev-nischay <nischay.mehn@gmail.com>
2025-10-03 20:50:46 +0530

    removed comments from schema

--- feed-back-mern/backend/model/db.js
+++ feed-back-mern/backend/model/db.js
@@ -2,32 +2,18 @@ import mongoose from "mongoose";
 const ObjectId = mongoose.ObjectId;
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
+const feedbackSchema = new Schema({
+  title: { type: String, required: true },
+  rating: { type: String, required: true },
+  userId: { type: ObjectId, required: true },
+  other: { type: String },
+});
 
-
-
-const userSchema = new Schema ({
-    email:{type:String,required:true,unique:true},
-    username:{type:String,required:true},
-    password:{type:String,required:true},
-})
-
-
-
-const feedbackSchema = new Schema ({
-    title:{type:String,required:true}, // how was your exp
-    rating:{type:String,required:true}, // event rating 
-    userId:{type:ObjectId,required:true}, // who published
-    other:{type:String} // any feedback they wanna give
-})
-
-
-
-
-export const User = new mongoose.model("user",userSchema)
-export const Feedback = new mongoose.model("feedback",feedbackSchema);
-
-
-
-
-
+export const User = new mongoose.model("user", userSchema);
+export const Feedback = new mongoose.model("feedback", feedbackSchema);
