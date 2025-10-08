a70650e82f60f8e30f948791a9dc93609431f113
dev-nischay <nischay.mehn@gmail.com>
2025-10-08 16:22:16 +0530

    fixed link schema

--- Remind/src/models/link-modal.ts
+++ Remind/src/models/link-modal.ts
@@ -1,9 +1,9 @@
 import mongoose, { Types } from "mongoose";
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const linkSchema = new Schema({
   hash: { type: String, required: true },
   userId: { type: Types.ObjectId, required: true, ref: "User" },
 });
 
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model("Link", linkSchema);
