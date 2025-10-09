cde1260cceee80654ddde01ba79ad96e82f3a276
dev-nischay <nischay.mehn@gmail.com>
2025-10-09 13:40:54 +0530

    updated models

--- Remind/src/models/content-modal.ts
+++ Remind/src/models/content-modal.ts
@@ -1,11 +1,7 @@
-import mongoose, { Types } from "mongoose";
-const Schema = mongoose.Schema;
-
-const contentTypes = ["image", "video", "article", "audio"]; // Extend as needed
+import mongoose, { Types, Schema } from "mongoose";
 
 const contentSchema = new Schema({
   link: { type: String, required: true },
-  type: { type: String, enum: contentTypes, required: true },
   title: { type: String, required: true },
   tags: [{ type: Types.ObjectId, ref: "Tag" }],
   userId: { type: Types.ObjectId, ref: "User", required: true },
