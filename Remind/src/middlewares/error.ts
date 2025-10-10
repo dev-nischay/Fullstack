58a5e4d473396b48f436ab53a9158f363fb4f7ed
dev-nischay <nischay.mehn@gmail.com>
2025-10-11 02:39:48 +0530

    added multiple error layers

--- Remind/src/middlewares/error.ts
+++ Remind/src/middlewares/error.ts
@@ -5,27 +5,43 @@ import type {
 } from "express";
 import { AppError } from "../utils/AppError.js";
 import { HttpStatus } from "../constants/enums.js";
+
 export const ErrorMiddleware = (
   err: unknown,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  if (err instanceof AppError) {
-    let statusCode = err.Statuscode || HttpStatus.INTERNAL_SERVER_ERROR;
-    if (err.isOperational) {
-      res.status(statusCode).json({
-        status: false,
-        error: err.message,
-      });
-    }else{
-console.error("Unexpected Error", err);
 
-    res.status(statusCode).json({
-      status: false,
-      error: `Something went wrong ${err.message}`,
-    });
-    }
+  if (err instanceof AppError) {
+
+    const statusCode = err.Statuscode || HttpStatus.INTERNAL_SERVER_ERROR;
     
+    if (err.isOperational) {
+
+        return res.status(statusCode).json({
+            status: false,
+            error: err.message,
+        });
+    } 
+    
+
+    console.error("UNEXPECTED NON-OPERATIONAL ERROR:", err); 
+    
+
+    return res.status(statusCode).json({
+        status: false,
+        error: "A critical server error occurred. Please report this issue.",
+    });
   }
+
+
+
+  console.error("UNHANDLED GENERIC ERROR:", err);
+
+
+  return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+    status: false,
+    error: "An unknown server error occurred. Please try again later.",
+  });
 };
