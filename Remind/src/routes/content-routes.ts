7dcc196fc8efff396e4efda45438a0eac5ab414c
dev-nischay <nischay.mehn@gmail.com>
2025-10-12 21:57:27 +0530

    added routes with controllers

--- Remind/src/routes/content-routes.ts
+++ Remind/src/routes/content-routes.ts
@@ -3,19 +3,22 @@ import { Validate } from "../middlewares/validate.js";
 import expressAsyncHandler from "express-async-handler";
 import { contentSchema, objectIdschema } from "../schema/general-schemas.js";
 import { auth } from "../middlewares/auth.js";
-import { createContent, delContent, deleteAllContent } from "../controllers/content-controller.js";
+import {
+  createContent,
+  delContent,
+  deleteAllContent,
+  getAllContent,
+} from "../controllers/content-controller.js";
 export const contentRouter = Router();
 
-
-
-
-
-contentRouter.use("/",auth)
-contentRouter.route('/').
-post(Validate(contentSchema),expressAsyncHandler(createContent))
-.delete(expressAsyncHandler(deleteAllContent))
-// .post(Validate(contentSchema),expressAsyncHandler(createContent))
-contentRouter.delete("/:id",Validate(objectIdschema,"params"),expressAsyncHandler(delContent))
-
-
-
+contentRouter.use("/", auth);
+contentRouter
+  .route("/")
+  .post(Validate(contentSchema), expressAsyncHandler(createContent))
+  .delete(expressAsyncHandler(deleteAllContent))
+  .get(expressAsyncHandler(getAllContent));
+contentRouter.delete(
+  "/:id",
+  Validate(objectIdschema, "params"),
+  expressAsyncHandler(delContent)
+);
