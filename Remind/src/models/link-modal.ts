97f9df378a538e030de00418d133ecf231662bb6
nischay <nischay.mehn@gmail.com>
2025-10-13 21:54:44 +0530

    Delete Remind/src directory
    fixed cloned repo

--- Remind/src/models/link-modal.ts
+++
@@ -1,9 +0,0 @@
-import mongoose, { Types } from "mongoose";
-const Schema = mongoose.Schema;
-
-const linkSchema = new Schema({
-  hash: { type: String, required: true },
-  userId: { type: Types.ObjectId, required: true, ref: "User" },
-});
-
-export const Link = mongoose.model("Link", linkSchema);
--- Remind/src/models/tags-modal.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { mongo } from "mongoose";
-const Schema = mongoose.Schema;
-
-const tagsSchema = new Schema({
-  title: { type: String, required: true, unique: true },
-});
-
-export const Tag = mongoose.model("Tag", tagsSchema);
